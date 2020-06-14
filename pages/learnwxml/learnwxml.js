e4790ff7ed381fc2e46225a629c551382247c435
Ezio <assassinste@gmail.com>
2020-06-14 23:12:14 +0800

    Latest learning code (related to if/for)

---
+++ pages/learnwxml/learnwxml.js
@@ -0,0 +1,12 @@
+//learnwxml.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    message: "Hello .wxml !",
+    item_id: 0,
+    definitely_true: true,
+    abc_num: [3, 2, 1],
+    my_name: "The OA"
+  },
+})
